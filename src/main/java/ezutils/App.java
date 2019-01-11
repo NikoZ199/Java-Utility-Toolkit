c33353990c7cf4092dd85e4390d1dd4e8ef1db4c
Levi <leevi.syvanen@hotmail.fi>
2019-01-11 02:40:46 +0200

    Make main method blank

--- src/main/java/ezutils/App.java
+++ src/main/java/ezutils/App.java
@@ -4,11 +4,7 @@
 package ezutils;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
     }
 }
